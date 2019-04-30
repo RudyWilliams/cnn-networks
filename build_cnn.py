e3f1c638dcf55af0965f478befcd58aeab59612f
Rudy <rudysw05@knights.ucf.edu>
2019-04-30 11:17:01 -0400

    initial commit

---
+++ build_cnn.py
@@ -0,0 +1,56 @@
+import tensorflow as tf
+from tensorflow import keras
+import digits_data as dd
+
+
+#hyperparams/initializations 
+batch_size = 100
+epochs = 12
+input_shape = (dd.width, dd.height, 1)
+loss = keras.losses.categorical_crossentropy
+optimizer = keras.optimizers.Adadelta()
+metrics = ['accuracy']
+
+
+model = keras.Sequential()
+
+c1 = keras.layers.Conv2D(filters=6,
+                   kernel_size=(5,5),
+                   padding='same',
+                   activation='relu',
+                   input_shape=input_shape)
+#input shape is only needed for first layer
+mp1 = keras.layers.MaxPool2D(pool_size=(2,2)) #reduces to 14x14
+## show what these images look like after this layer
+model.add(c1)
+model.add(mp1)
+
+c2 = keras.layers.Conv2D(filters=16,
+                   kernel_size=(5,5),
+                   padding='valid', #reduces to 10x10
+                   activation='relu')
+mp2 = keras.layers.MaxPool2D(pool_size=(2,2)) #reduces to 5x5
+model.add(c2)
+model.add(mp2)
+
+model.add(keras.layers.Flatten()) #shape of 5x5 x16 feature maps = 400
+fc1 = keras.layers.Dense(120, activation='relu')
+fc2 = keras.layers.Dense(84, activation='relu')
+output_layer = keras.layers.Dense(dd.n_classes, activation='softmax')
+model.add(fc1)
+model.add(fc2)
+model.add(output_layer)
+
+model.compile(loss=loss, optimizer=optimizer, metrics=metrics)
+
+model.fit(dd.image_tr_rshp, dd.y_tr_hot,
+              batch_size=batch_size,
+              epochs=epochs,
+              verbose=1,
+              validation_data=(dd.image_te_rshp, dd.y_te_hot))
+
+score = model.evaluate(dd.image_te_rshp, dd.y_te_hot, verbose=0)
+
+if __name__ == '__main__':
+    print(f'Test loss: {score[0]}')
+    print(f'Test accuracy: {score[1]}')
---
+++ digits_data.py
@@ -0,0 +1,50 @@
+"""
+    This module loads the data.
+    Provide a clean way to access the raw data (for showing humans).
+    Provide a way to access manipulated shapes (for showing tf).
+"""
+import tensorflow as tf
+
+
+data = tf.keras.datasets.mnist.load_data()
+train, test = data
+image_train, y_train = train
+image_test, y_test = test
+
+n_classes = 10
+width, height = image_train.shape[1::]
+
+image_tr_rshp = image_train.reshape(-1, width, height, 1)
+image_te_rshp = image_test.reshape(-1, width, height, 1)
+y_tr_hot = tf.keras.utils.to_categorical(y_train, 10)
+y_te_hot = tf.keras.utils.to_categorical(y_test, 10)
+
+if __name__ == '__main__':
+
+    import matplotlib.pyplot as plt
+
+    print(f'Running TensorFlow v {tf.__version__}')
+    print(f'Training data: {image_train.shape[1::]}')
+    print(f'Training labels: {y_train.shape}')
+    print(f'Test data: {image_test.shape}')
+    print(f'Test labels: {y_test.shape}')
+    ## 60,000 training images of shape (28, 28, 1) #bw images
+    ## 10,000 test images
+
+    #plot an image to show the datas form
+    #plt.figure(figsize=(1,2))
+    plt.imshow(image_train[0])
+    plt.colorbar()
+    plt.grid(False)
+    plt.show()
+    ## this shows that each pixel takes a value from 0 to 255
+
+    #make each value be from 0 to 1
+    image_train = image_train / 255
+    image_test = image_test / 255
+
+    plt.imshow(image_train[0])
+    plt.colorbar()
+    plt.grid(False)
+    plt.show()
+    
