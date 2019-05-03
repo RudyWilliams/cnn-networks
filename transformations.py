49381ced23de92d37bed6ab793ce517bd13f4871
Rudy <rudysw05@knights.ucf.edu>
2019-05-03 10:19:10 -0400

    easier to change what is plotted

--- transformations.py
+++ transformations.py
@@ -5,6 +5,46 @@ from tensorflow import keras
 from tensorflow.keras import backend as K 
 from digits_data import image_train, image_tr_rshp
 
+
+class CreateRow:
+    """ Class to visualize the transformations that take place due to C1.
+
+        Args:
+            images: np.array - the data containing the original images
+                *shape=(n_images, height_of_image, width_of_image)
+            kernels: list - the list of kernels
+            feature_maps: list - the list of feature maps
+
+        Returns:
+            instance of a 6x3 subplot
+
+        Methods:
+            new_row:
+                Args:
+                    row_num: int - valid values are 0-5
+                    im_loc: int - the image to show (keep consistent)
+                    ker_fmap_loc: int - the kernel and corresponding f_map
+                                 to plot
+
+                Returns:
+                    updated instance of the 6x3 subplot
+    """
+
+    def __init__(self, images, kernels, feature_maps):
+        self.fig, self.axs = plt.subplots(6, 3)
+        self.images = images
+        self.kernels = kernels
+        self.feature_maps = feature_maps
+
+    def new_row(self,  row_num, im_loc, ker_fmap_loc):
+        imc0 = self.axs[row_num, 0].imshow(self.images[im_loc], cmap='binary')
+        self.fig.colorbar(imc0, ax=self.axs[row_num, 0])
+        imc1 = self.axs[row_num, 1].imshow(self.kernels[ker_fmap_loc], cmap='binary') #don't want to norm this one bc the neg values are valueable
+        self.fig.colorbar(imc1, ax=self.axs[row_num, 1])
+        imc2 = self.axs[row_num, 2].imshow(self.feature_maps[ker_fmap_loc], cmap='binary', vmin=0, vmax=1)
+        self.fig.colorbar(imc2, ax=self.axs[row_num, 2])
+
+
 lenet5 = tf.keras.models.load_model('lenet_5.h5py')
 
 #get weights of c1 (i.e. get the kernels)
@@ -19,58 +59,17 @@ X = np.expand_dims(X, axis=0)
 c1_output = K.function([c1.input],
                        [c1.output])
 
-
 fmaps = [c1_output(X)[0][:, :, :, i][0] for i in range(c1_W.shape[3])]
 
 if __name__ == '__main__':
     
-    fig, axs = plt.subplots(6, 3)
-
-    im00 = axs[0, 0].imshow(image_train[0], cmap='binary')
-    fig.colorbar(im00, ax=axs[0,0])
-    im01 = axs[0, 1].imshow(kernels[0], cmap='binary') #don't want to norm this one bc the neg values are valueable
-    fig.colorbar(im01, ax=axs[0,1])
-    im02 = axs[0, 2].imshow(fmaps[0], cmap='binary', vmin=0, vmax=1)
-    fig.colorbar(im02, ax=axs[0,2])
-
-    im10 = axs[1, 0].imshow(image_train[0], cmap='binary')
-    fig.colorbar(im10, ax=axs[1,0])
-    im11 = axs[1, 1].imshow(kernels[1], cmap='binary') #don't want to norm this one bc the neg values are valueable
-    fig.colorbar(im11, ax=axs[1,1])
-    im12 = axs[1, 2].imshow(fmaps[1], cmap='binary', vmin=0, vmax=1)
-    fig.colorbar(im12, ax=axs[1,2])
-
-    im20 = axs[2, 0].imshow(image_train[0], cmap='binary')
-    fig.colorbar(im20, ax=axs[2,0])
-    im21 = axs[2, 1].imshow(kernels[2], cmap='binary') #don't want to norm this one bc the neg values are valueable
-    fig.colorbar(im21, ax=axs[2,1])
-    im22 = axs[2, 2].imshow(fmaps[2], cmap='binary', vmin=0, vmax=1)
-    fig.colorbar(im22, ax=axs[2,2])
-
-    im30 = axs[3, 0].imshow(image_train[0], cmap='binary')
-    fig.colorbar(im30, ax=axs[3,0])
-    im31 = axs[3, 1].imshow(kernels[3], cmap='binary') #don't want to norm this one bc the neg values are valueable
-    fig.colorbar(im31, ax=axs[3,1])
-    im32 = axs[3, 2].imshow(fmaps[3], cmap='binary', vmin=0, vmax=1)
-    fig.colorbar(im32, ax=axs[3,2])
-
-    im40 = axs[4, 0].imshow(image_train[0], cmap='binary')
-    fig.colorbar(im40, ax=axs[4,0])
-    im41 = axs[4, 1].imshow(kernels[4], cmap='binary') #don't want to norm this one bc the neg values are valueable
-    fig.colorbar(im41, ax=axs[4,1])
-    im42 = axs[4, 2].imshow(fmaps[4], cmap='binary', vmin=0, vmax=1)
-    fig.colorbar(im42, ax=axs[4,2])
-
-    im50 = axs[5, 0].imshow(image_train[0], cmap='binary')
-    fig.colorbar(im50, ax=axs[5,0])
-    im51 = axs[5, 1].imshow(kernels[5], cmap='binary') #don't want to norm this one bc the neg values are valueable
-    fig.colorbar(im51, ax=axs[5,1])
-    im52 = axs[5, 2].imshow(fmaps[5], cmap='binary', vmin=0, vmax=1)
-    fig.colorbar(im52, ax=axs[5,2])
-
-
-
+    print(image_train.shape)
+    grid = CreateRow(image_train, kernels, fmaps)
+    grid.new_row(row_num=0, im_loc=0, ker_fmap_loc=0)
+    grid.new_row(row_num=1, im_loc=0, ker_fmap_loc=1)
+    grid.new_row(row_num=2, im_loc=0, ker_fmap_loc=2)
+    grid.new_row(row_num=3, im_loc=0, ker_fmap_loc=3)
+    grid.new_row(row_num=4, im_loc=0, ker_fmap_loc=4)
+    grid.new_row(row_num=5, im_loc=0, ker_fmap_loc=5)
 
     plt.show()
-    
-    
